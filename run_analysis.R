887d10a11af777135a5b7ce44cc4b2516bbf20f3
jingliu515 <jingliu515@gmail.com>
2015-04-26 20:29:08 -0500

    update

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@ getwd()
 
 if(!file.exists("courseraData")) {dir.create("courseraData")}
 fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileurl,destfile="./courseraData/getdata_projectfiles_UCI HAR Dataset.zip")
+download.file(fileURL,destfile="./courseraData/getdata_projectfiles_UCI HAR Dataset.zip")
 
 file<-"./courseraData/getdata_projectfiles_UCI HAR Dataset.zip"
 
